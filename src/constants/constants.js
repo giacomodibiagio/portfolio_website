399133d1f4dc41509812b4e90d7387c52103ecc5
Giacomo_88 <dibiagio.giacomo@gmail.com>
2022-12-30 17:16:41 +0100

    fix added tags to angular exercises

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -12,7 +12,7 @@ export const projects = [
     title: 'Angular exercises',
     description: 'An app to train Angular, Typescript and Javascript mechanics. Click and train right away with the app code and a dedicated mock server!',
     image: '/images/2.png', //add image png or jpg
-    tags: ['Angular', 'MockServer', 'Netlify'],
+    tags: ['Angular', 'Mockettaro', 'MockServer', 'Netlify'],
     source: 'https://github.com/search?q=user%3Agiacomodibiagio+angular-exercises', //link to source code on github
     visit: 'https://giacomodibiagioangularexercises.netlify.app/', //link to project deployed
     id: 1,
